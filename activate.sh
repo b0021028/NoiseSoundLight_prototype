eaa74c0ea862a85b3ad0ce348954f1965ae537ff
b0021028 <108704791+b0021028@users.noreply.github.com>
2024-02-16 08:23:40 +0900

    commit

--- activate.sh
+++ activate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo pigpiod
-source "$(dirname "$0")/.venv/bin/activate"
-python3 "$(dirname "$0")/prototype20240129/core.py"
+source .venv/bin/activate
+python3 prototype20240129/core.py
--- setup.sh
+++ setup.sh
@@ -9,18 +9,15 @@ cd pigpio-79
 
 make
 sudo make install
-# If the Python part of the install fails it may be because you need the setup tools.
-#sudo apt install python-setuptools python3-setuptools
-# end install
 
 # check liblary
-./x_pigpio # check C I/F
+sudo ./x_pigpio # check C I/F
 
 sudo pigpiod    # start daemon
 
-./x_pigpio.py   # check Python I/F to daemon
+sudo ./x_pigpio.py   # check Python I/F to daemon
 
 cd ..
 python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install -U -y sounddevice
+python3 -m pip install -U sounddevice numpy pigpio
