fdc1aa8f19db30bec84abfac48f48678adf7b8b6
b0021028 <108704791+b0021028@users.noreply.github.com>
2024-02-16 07:40:05 +0900

    Update pigpio_setup.sh

--- pigpio_setup.sh
+++ pigpio_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install python3-setuptools python3-pip python3-venv
+sudo apt install python3-setuptools python3-pip python3-venv portaudio19-dev
 
 # pigpio install
 wget https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
@@ -8,7 +8,7 @@ tar -xvf v79.tar.gz
 cd pigpio-79
 
 make
-make install
+sudo make install
 # If the Python part of the install fails it may be because you need the setup tools.
 #sudo apt install python-setuptools python3-setuptools
 # end install
@@ -16,16 +16,11 @@ make install
 # check liblary
 ./x_pigpio # check C I/F
 
-pigpiod    # start daemon
+sudo pigpiod    # start daemon
 
-./x_pigpiod_if2 # check C      I/F to daemon
 ./x_pigpio.py   # check Python I/F to daemon
-./x_pigs        # check pigs   I/F to daemon
-./x_pipe        # check pipe   I/F to daemon
 
-killall pigpiod
-pigpiod
-cd ~
+cd ..
 python3 -m venv .venv
 source .venv/bin/activate
-pip install sounddevice
+python3 -m pip install sounddevice
