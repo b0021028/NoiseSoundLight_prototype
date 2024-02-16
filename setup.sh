e1d2094a1bf38fd1136b60e15efa53c594a3ec3c
b0021028 <108704791+b0021028@users.noreply.github.com>
2024-02-16 17:40:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 sudo DEBIAN_FRONTEND=nointeractive apt install -y python3-setuptools python3-pip python3-venv portaudio19-dev python3-pigpio
 
 # pigpio install
-# wget https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
-# tar -xvf v79.tar.gz
-# cd pigpio-79
+wget https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
+tar -xvf v79.tar.gz
+cd pigpio-79
 
-# make
-# sudo make install
+make
+sudo make install
 
 # # check liblary
 # sudo ./x_pigpio # check C I/F
@@ -17,7 +17,7 @@ sudo DEBIAN_FRONTEND=nointeractive apt install -y python3-setuptools python3-pip
 
 # sudo ./x_pigpio.py   # check Python I/F to daemon
 # sudo apt install pigpio python3-pigpio
-# cd ..
+cd ..
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -U sounddevice numpy pigpio
